56709ae5993ccc6472ab2a6f54794548c67e12ad
Sagnnik Biswas <sagnnikbiswas2002@gmail.com>
2025-10-17 15:55:02 +0000

    Added clerk auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,19 @@ import PostPreview from './pages/PostPreview';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import About from "./pages/About";
 import Tags from "./pages/Tags";
+import AdminRoute from './components/AdminRoute';
+import PublicBlogPage from './pages/PublicBlogPage';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AdminBlogPage />} />
-        <Route path= "/about" element={<About />} />
-        <Route path= "/tags" element={<Tags />} />
-        <Route path='/admin/publish/:postId' element={<PublishHtml />} />
-        <Route path='/article/:slug' element={<PostPreview />} />
+        <Route path='/' element={<PublicBlogPage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/tags' element={<Tags />} />
+        <Route path='/admin/publish/:postId' element={<AdminRoute><PublishHtml /></AdminRoute>} />
+        <Route path='/admin' element={<AdminRoute><AdminBlogPage /></AdminRoute>} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/PublicBlogPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PublicBlogPage() {
+  return (
+    <div>
+      <p>This is the public blog page</p>
+    </div>
+  )
+}
