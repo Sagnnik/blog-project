1790fdf441ec6009d22a5acfdccd5974487a0189
Sagnnik Biswas <sagnnikbiswas2002@gmail.com>
2025-10-29 19:12:52 +0000

    updated dockerfile

--- backend/app/run_server.sh
+++
@@ -1,9 +0,0 @@
-!/usr/bin/env bash
-echo "Starting servers in new terminal windows..."
-
-gnome-terminal -- bash -c "echo 'Starting Uvicorn...'; uvicorn main:app --reload; exec bash"
-
-
-gnome-terminal -- bash -c "echo 'Starting HTTP Server...'; npx http-server ./uploads -p 8001 --cors; exec bash"
-
-echo "Both servers started in separate windows!"
